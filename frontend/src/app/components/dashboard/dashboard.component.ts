f741c21a13a9498029e390f484cfd65a57ebeef9
tbalint19 <toth910719balint@gmail.com>
2017-07-03 12:14:38 +0200

    endpoint mocks added

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {GlobalEventsManager} from "../../global.eventsmanager";
+import {HttpClient} from "../../_httpclient/httpclient";
+import {Location} from "../../_models/index";
 
 @Component({
     moduleId: module.id,
@@ -9,11 +11,17 @@ import {GlobalEventsManager} from "../../global.eventsmanager";
 })
 export class DashboardComponent {
 
-    constructor(private router: Router,
-                private eventsManager: GlobalEventsManager) {
+    public locations: Location[];
 
-        this.eventsManager.showNavBar(true);
-
+    constructor(
+        private client: HttpClient,
+        private router: Router,
+        private eventsManager: GlobalEventsManager) {
+            this.eventsManager.showNavBar(true);
     }
+
+    // getLocations(){
+    //     this.client.get()
+    // }
 
 }
