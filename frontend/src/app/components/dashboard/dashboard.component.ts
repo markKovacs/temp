1b37f86a045b1f103111fe9e6ed433ffb73068b9
tbalint19 <toth910719balint@gmail.com>
2017-07-03 15:38:25 +0200

    dashboard fetces data based on location

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
-import {Component} from '@angular/core';
-import {Router} from '@angular/router';
-import {GlobalEventsManager} from "../../global.eventsmanager";
-import {HttpClient} from "../../_httpclient/httpclient";
-import {Location} from "../../_models/index";
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { GlobalEventsManager } from "../../global.eventsmanager";
+import { HttpClient } from "../../_httpclient/httpclient";
+import { Location, User } from "../../_models/index";
 
 @Component({
     moduleId: module.id,
@@ -11,17 +11,59 @@ import {Location} from "../../_models/index";
 })
 export class DashboardComponent {
 
-    public locations: Location[];
+    public locations: Location[] = [];
+    public usersWithVideo: User[] = [];
+    public usersWithScreening: User[] = [];
 
     constructor(
         private client: HttpClient,
         private router: Router,
         private eventsManager: GlobalEventsManager) {
             this.eventsManager.showNavBar(true);
+            this.getLocations();
+            this.getUsersWithVideo();
+            this.getUsersWithScreening();
     }
 
-    // getLocations(){
-    //     this.client.get()
-    // }
+    getLocations() {
+        this.client.get('api/locations').subscribe(
+            (locations: Location[]) => { for (let location of locations) { this.locations.push(location) }},
+            (error) => { console.log(error); },
+            () => { console.log("GET - api/locations"); }
+        )
+    }
+
+    getUsersWithVideo() {
+        let id = JSON.parse(localStorage.chosenLocation).id
+        this.client.get('api/dashboard/motivation?location=' + id).subscribe(
+          (users: User[]) => this.usersWithVideo = users,
+          (error) => { console.log(error); },
+          () => { console.log("GET - api/dashboard/motivation") }
+        )
+    }
+
+    getUsersWithScreening() {
+        let id = JSON.parse(localStorage.chosenLocation).id
+        this.client.get('api/dashboard/screening?location=' + id).subscribe(
+          (users: User[]) => this.usersWithScreening = users,
+          (error) => { console.log(error); },
+          () => { console.log("GET - api/dashboard/screening") }
+        )
+    }
+
+    chooseLocation(id) {
+        let chosen = this.locations.filter((location) => location.id == id)[0];
+        localStorage.chosenLocation = JSON.stringify(chosen);
+        this.getUsersWithVideo();
+        this.getUsersWithScreening();
+    }
+
+    locationChosen() {
+      return localStorage.chosenLocation != undefined;
+    }
+
+    isChosen(id) {
+      return id == JSON.parse(localStorage.chosenLocation).id ? "selected-location" : "unselected-location";
+    }
 
 }
