3b07b10975439e2ed6dab17480cb2316fa6b6cac
Ivanics Szilárd <ivanics.szilard@gmail.com>
2017-07-17 14:59:09 +0200

    Location Rest controller Question model with correct answers

--- backend/src/main/java/com/codecool/appsystem/admin/controller/LocationsRestController.java
+++ backend/src/main/java/com/codecool/appsystem/admin/controller/LocationsRestController.java
@@ -1,32 +1,76 @@
 package com.codecool.appsystem.admin.controller;
 
-import com.codecool.appsystem.admin.model.Location;
+import com.codecool.appsystem.admin.model.*;
 import com.codecool.appsystem.admin.model.dto.QuestionDTO;
 import com.codecool.appsystem.admin.repository.LocationRepository;
+import com.codecool.appsystem.admin.repository.TestAnswerRepository;
+import com.codecool.appsystem.admin.repository.TestRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import junit.framework.TestResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-@RestController(value = "/api/locations")
+@Slf4j
+
+@RestController
+@RequestMapping("/api/locations")
 public class LocationsRestController {
 
     @Autowired
     private LocationRepository locationRepository;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @Autowired
+    private TestRepository testRepository;
+
+    @Autowired
+    private TestAnswerRepository testAnswerRepository;
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
     public List<Location> getAllLocations() {
         return locationRepository.findAll();
     }
 
+
     @RequestMapping(value="/test/{location}", method = RequestMethod.GET)
-    public List<QuestionDTO> getQuestionByLocation(@PathVariable("location") String location) {
+    public List<Question> getQuestionByLocation(@PathVariable("location") String location) throws IOException {
+        log.trace("LOCATION CONTROLLER");
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<QuestionDTO> result = new ArrayList<>();
+        List<Test> testsByLocation = testRepository.findByLocationIdOrderByOrderInBundleAsc(location);
+        List<Question> questions = new ArrayList<>();
+        for (Test test : testsByLocation) {
+            questions.add(mapper.readValue(test.getFormAsJson(), Question.class));
+            log.trace("question" + mapper.readValue(test.getFormAsJson(), Question.class));
+        }
+
+        for (Question question : questions) {
+            for (QuestionContent qC : question.getQuestions()) {
+                List<String> testAnswerOptionId = testAnswerRepository.findByQuestionIdOrderByCorrectAnswerAsc(qC.getId()).stream().map(TestAnswer::getCorrectAnswer).collect(Collectors.toList());
+                log.trace("testAnser" + testAnswerOptionId);
+                for (QuestionOption qO : qC.getOptions()) {
+                    if (testAnswerOptionId.contains(qO.getId())){
+                        qO.setIsCorrect(true);
+                        log.trace("Option Answer is Correct: " + qO);
+                    } else{
+                        qO.setIsCorrect(false);
+                        log.trace("Option Answer is False: " + qO);
+                    }
+                }
+            }
+        }
 
 
-        return null;
+        return questions;
     }
 
 
--- backend/src/main/java/com/codecool/appsystem/admin/model/dto/QuestionDTO.java
+++ backend/src/main/java/com/codecool/appsystem/admin/model/dto/QuestionDTO.java
@@ -1,4 +1,35 @@
 package com.codecool.appsystem.admin.model.dto;
 
+import com.codecool.appsystem.admin.model.Question;
+import com.codecool.appsystem.admin.model.QuestionContent;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sun.xml.internal.bind.v2.runtime.InlineBinaryTransducer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class QuestionDTO {
+
+    private String id;
+    private String name;
+    private String locationId;
+    private Integer orderInBundle;
+    private String description;
+    private String surveyContent;
+    private Integer maxPoints;
+    private Integer threshold;
+    private Boolean enabled;
+    private Integer estimatedTime;
+    private Boolean motivationVideo;
+    protected List<QuestionContent> questions;
+
+
 }
