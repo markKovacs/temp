8bb607b4c1df14d73fbb9bb02abb332ed719b176
tbalint19 <toth910719balint@gmail.com>
2017-07-31 14:50:52 +0200

    all functions

--- frontend/src/app/components/template-editor/template-editor.component.ts
+++ frontend/src/app/components/template-editor/template-editor.component.ts
@@ -4,6 +4,7 @@ import {GlobalEventsManager} from "../../global.eventsmanager";
 import {Location, Template, PostResponse} from "../../_models/index";
 import {Message} from 'primeng/primeng';
 import Mustache from 'mustache/mustache';
+import * as _ from 'lodash';
 
 @Component({
     moduleId: module.id,
@@ -15,7 +16,9 @@ export class TemplateEditorComponent {
     public location: Location;
     public templates: Template[];
     public chosenTemplate: Template;
+    public masterTemplate: Template;
     public messages: Message[] = [];
+    public newKey: string;
 
     constructor(
         private client: HttpClient,
@@ -32,7 +35,13 @@ export class TemplateEditorComponent {
 
     getTemplates(){
         this.client.get('/api/templates?location=' + this.location.id).subscribe(
-            (templates: Template[]) => this.templates = templates
+            (templates: Template[]) => {
+                this.templates = templates;
+                for (let template of this.templates) {
+                    template.model = template.model == null ? {} : JSON.parse(template.model);
+                }
+                this.masterTemplate = this.templates.filter(entry => entry.master)[0]
+            }
         )
     }
 
@@ -40,14 +49,30 @@ export class TemplateEditorComponent {
         this.chosenTemplate = template;
     }
 
+    getKeys(model){
+        return _.keys(model)
+    }
+
+    addNewKey(){
+        this.chosenTemplate.model[this.newKey] = null;
+        this.newKey = null;
+    }
+
     previewTemplate(template){
-      let generated = Mustache.render(template, {name: "bela"});
-      let newWindow = window.open("", "Title", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=800, height=600, top=0, left=0");
-      newWindow.document.body.innerHTML = generated;
+        let generated = Mustache.render(template, this.chosenTemplate.model);
+        let parts = this.masterTemplate.template.split("({[content]})");
+        let full = parts[0] + generated + parts[1];
+        let newWindow = window.open("", "", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=800, height=700, top=0, left=0");
+        newWindow.document.body.innerHTML = full;
     }
 
     saveTemplate(template){
-        this.client.post('/api/templates/save', template).subscribe(
+        let postData = Object.assign({}, template);
+        for (let key in postData.model) {
+            postData.model[key] = null;
+        }
+        postData.model = JSON.stringify(postData.model);
+        this.client.post('/api/templates/save', postData).subscribe(
             (response: PostResponse) => this.messages.push(
                 {
                     severity: 'success',
