56f20dc82f4331b8add7cb8ad4258cbd67799f10
Balazs Pekar <peba0706@gmail.com>
2017-08-23 15:34:00 +0200

    giving some looks to template editor

--- frontend/src/app/components/template-editor/template-editor.component.ts
+++ frontend/src/app/components/template-editor/template-editor.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {HttpClient} from '../../_httpclient/httpclient';
 import {Location, Template, PostResponse} from '../../_models/index';
-import {Message} from 'primeng/primeng';
 import Mustache from 'mustache/mustache';
 import * as _ from 'lodash';
 import {EmailTemplateService} from '../../_services/email-template.service';
@@ -17,9 +16,7 @@ export class TemplateEditorComponent {
     public templates: Template[];
     public masterTemplate: Template;
     public selectedTemplate: Template;
-    public newKey: string;
     editingName = false;
-
     public options: Object = {
         immediateAngularModelUpdate: true
     };
@@ -27,56 +24,69 @@ export class TemplateEditorComponent {
     constructor(private client: HttpClient,
                 private emailTemplateService: EmailTemplateService) {
         this.location = JSON.parse(localStorage.getItem('chosenLocation'));
+        this.getTemplates();
+    }
+
+    getTemplates(): void {
         this.emailTemplateService.getTemplates(this.location.id)
             .subscribe(
                 (templates: Template[]) => {
-                this.templates = templates;
-                for (const template of this.templates) {
-                    template.model = (template.model == null) ? {} : JSON.parse(template.model);
+                    this.templates = templates;
+                    for (const template of this.templates) {
+                        template.model = (template.model == null) ? {} : JSON.parse(template.model);
+                    }
+                    this.masterTemplate = this.templates.filter(element => element.master)[0];
+                });
+    }
+
+    saveTemplate(template): void {
+        const copy = Object.assign({}, template);
+        copy.model = JSON.stringify(copy.model);
+        this.emailTemplateService.saveTemplate(copy)
+            .subscribe(
+                (response: PostResponse) => {
+                    if (response.success) {
+                        // todo show msg growl with success severity
+                    } else {
+                        // todo show msg growl with error severity
+                    }
                 }
-                this.masterTemplate = this.templates.filter(element => element.master)[0];
-            });
+            );
     }
 
     selectTemplate(template) {
         this.selectedTemplate = template;
     }
 
-    getKeys(model) {
+    getKeys(model: any): any[] {
         return _.keys(model)
     }
 
-    addNewKey() {
-        this.selectedTemplate.model[this.newKey] = null;
-        this.newKey = null;
-    }
-
-    previewTemplate(template) {
+    openPreviewWindow(template) {
         const generated = Mustache.render(template, this.selectedTemplate.model);
         const generatedMaster = Mustache.render(this.masterTemplate.template, this.masterTemplate.model);
-        const parts = generatedMaster.split('({[content]})');
-        if (parts.length !== 2) {
-            // this.messages.push({
-            //     severity: 'error',
-            //     summary: 'Error in Master template',
-            //     detail: 'Please check the syntax and try again'
-            // });
-        }
-        const full = parts[0] + generated + parts[1];
-        const newWindow = window.open('', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=800, height=700, top=0, left=0');
-        newWindow.document.body.innerHTML = full;
-    }
+        const commonTemplateParts = generatedMaster.split('({[content]})');
 
-    saveTemplate(template) {
-        let copy = Object.assign({}, template);
-        copy.model = JSON.stringify(copy.model);
-        console.log(copy);
-        this.emailTemplateService.saveTemplate(copy)
-            .subscribe(
-                (response: PostResponse) => {
-                    response.success ? console.log('saved') : console.log('something went wrong');
-                }
-            );
+        if (commonTemplateParts.length !== 2) {
+            // todo show msg growl with error severity
+        }
+
+        const full = commonTemplateParts[0] + generated + commonTemplateParts[1];
+        const newWindow = window.open(
+            '',
+            '',
+            'toolbar=no,' +
+            'location=no,' +
+            'directories=no,' +
+            'status=no,' +
+            'menubar=no,' +
+            'scrollbars=yes,' +
+            'resizable=yes,' +
+            'width=800,' +
+            'height=700,' +
+            'top=0,' +
+            'left=0');
+        newWindow.document.body.innerHTML = full;
     }
 
 }
