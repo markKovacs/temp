25fe4efdb8a40a2c622bc667ad3c0482ef85d102
Balazs Pekar <peba0706@gmail.com>
2017-09-14 13:28:55 +0200

    a whole bunch of refactor mainly naming and new functs

--- frontend/src/app/components/calendar/calendar.component.ts
+++ frontend/src/app/components/calendar/calendar.component.ts
@@ -1,16 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {GlobalEventsManager} from "../../global.eventsmanager";
-import {HttpClient} from "../../_httpclient/httpclient";
-import {Location, User} from "../../_models/index";
-import {ScreeningInfo} from "../../_models/screeninginfo.model";
-import {ScreeningService} from "../../_services/screening.service";
-import { DatePipe } from '@angular/common';
+import {GlobalEventsManager} from '../../global.eventsmanager';
+import {ScreeningInfo} from '../../_models/screeninginfo.model';
+import {ScreeningService} from '../../_services/screening.service';
+import {DatePipe} from '@angular/common';
 import {Message, SelectItem} from 'primeng/primeng';
-import {DateFormatPipe} from "angular2-moment";
-import {PostResponse} from "../../_models/post-response.model";
-import {isNullOrUndefined} from "util";
-import {AlertService} from "../../_services/alert.service";
+import {isNullOrUndefined} from 'util';
+import {AlertService} from '../../_services/alert.service';
 
 
 @Component({
@@ -20,75 +16,92 @@ import {AlertService} from "../../_services/alert.service";
 })
 export class CalendarComponent implements OnInit {
 
-    hu: any;
-
-    users: ScreeningInfo[] = [];
-
+    calendarLocalization: any; // used for primeNG calendar to start the weeks with Mondays by default its Sundays
+    candidates: ScreeningInfo[] = [];
     selectDates: SelectItem[] = [];
+    screeningDates: Date[] = [];
+    date: Date;
+    loading = false;
 
-    public dates: Date[] = [];
-    public date: Date;
-    public messages: Message[] = [];
-    minDate = new Date();
-
-    loading: boolean = false;
-
-    constructor(private router: Router,
-                private eventsManager: GlobalEventsManager,
-                private screeningService: ScreeningService,
-                private dateFormatter: DatePipe,
-                private alertService: AlertService
+    constructor(
+        private eventsManager: GlobalEventsManager,
+        private screeningService: ScreeningService,
+        private dateFormatter: DatePipe,
+        private alertService: AlertService
     ) {
-        this.loading = true;
+        // this.loading = true;
         this.eventsManager.showNavBar(true);
-        this.fetchUsers();
+        this.fetchCandidates();
     }
 
-    ngOnInit() {
-        this.hu = {
+    ngOnInit(): void {
+        this.setCalendarLocalization();
+        this.candidates.filter(candidate => !isNullOrUndefined(candidate.groupTime));
+    }
+
+    private setCalendarLocalization(): void {
+        this.calendarLocalization = {
             firstDayOfWeek: 1,
-            dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-            dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-            dayNamesMin: ["Su","Mo","Tu","We","Th","Fr","Sa"],
-            monthNames: [ "January","February","March","April","May","June","July","August","September","October","November","December" ],
-            monthNamesShort: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ]
+            dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            dayNamesMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+            monthNamesShort: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
+            monthNames: [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December'
+            ]
         };
-        this.users.filter(u => !isNullOrUndefined(u.groupTime));
     }
 
-    private fetchUsers() {
-
-        this.screeningService.findAssignmentCandidates().subscribe(
+    private fetchCandidates(): void {
+        this.screeningService.findAssignmentCandidates()
+            .subscribe(
             (data: ScreeningInfo[]) => {
-                this.users = data;
-                this.loadDates();
+                this.candidates = data;
+                console.log('this.candidates = ', this.candidates);
+                this.setScreeningDates();
+                // this.loadDates();
             },
             error => console.log(error)
         );
-
     }
 
-    private loadDates() {
+    private setScreeningDates(): void {
+        this.candidates.forEach(candidate => {
+            if (candidate.groupTime != null && !this.screeningDates.includes(candidate.groupTime)) {
+                this.screeningDates.push(candidate.groupTime);
+            }
+        });
+    }
 
-        this.users.forEach(user => {
-            if (user.groupTime != null && !this.dates.includes(user.groupTime)) {
-                this.dates.push(user.groupTime);
+    private loadDates(): void {
+        this.candidates.forEach(candidate => {
+            if (candidate.groupTime != null && !this.screeningDates.includes(candidate.groupTime)) {
+                this.screeningDates.push(candidate.groupTime);
                 this.selectDates.push(
                     {
-                        label: this.dateFormatter.transform(user.groupTime, 'yyyy.MM.dd. HH:mm'),
-                        value: user.groupTime
+                        label: this.dateFormatter.transform(candidate.groupTime, 'yyyy.MM.dd. HH:mm'),
+                        value: candidate.groupTime
                     }
                 );
             }
         });
-
         this.loading = false;
-
     }
 
     addDate() {
-        if (this.date && !this.dates.includes(this.date)) {
-            this.dates.push(this.date);
+        if (this.date && !this.screeningDates.includes(this.date)) {
+            this.screeningDates.push(this.date);
             this.selectDates.push(
                 {
                     label: this.dateFormatter.transform(this.date, 'yyyy.MM.dd. HH:mm'),
@@ -97,34 +110,35 @@ export class CalendarComponent implements OnInit {
             );
         }
         this.date = null;
-        this.dates.sort();
+        this.screeningDates.sort();
     }
 
-    saveGroupTimes(){
-        this.screeningService.saveGroupTimes(this.users).subscribe(
-            (data: any) => {},
-            error2 => console.log(error2)
-        );
+    saveGroupTimes(): void {
+        this.screeningService.saveGroupTimes(this.candidates)
+            .subscribe(
+                (data: any) => { },
+                error => console.log(error)
+            );
     }
 
-    savePersonalTimes(){
-
+    savePersonalTimes(): void {
         let missingGroupTimes = false;
 
-        for(let u of this.users){
-            if(!u.groupTime){
+        for (const u of this.candidates) {
+            if (!u.groupTime) {
                 missingGroupTimes = true;
             }
         }
 
-        if(missingGroupTimes){
-            this.alertService.showAlert("error", "Missing group time", "Assign group times to all applicants");
+        if (missingGroupTimes) {
+            this.alertService.showAlert('error', 'Missing group time', 'Assign group times to all applicants');
             return;
         }
 
-        this.screeningService.savePersonalTimes(this.users).subscribe(
-            (data: any) => {},
-            error2 => console.log(error2)
-        );
+        this.screeningService.savePersonalTimes(this.candidates)
+            .subscribe(
+                (data: any) => {},
+                error => console.log(error)
+            );
     }
 }
