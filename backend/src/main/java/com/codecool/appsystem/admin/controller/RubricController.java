0806c7632e502a2a9ab48534e3f40deea958f6d3
castarox <mika.kamil12@gmail.com>
2017-11-16 09:04:27 +0100

    Update RubricController to use RubricDTO

--- backend/src/main/java/com/codecool/appsystem/admin/controller/RubricController.java
+++ backend/src/main/java/com/codecool/appsystem/admin/controller/RubricController.java
@@ -1,12 +1,12 @@
 package com.codecool.appsystem.admin.controller;
 
 import com.codecool.appsystem.admin.model.Rubric;
+import com.codecool.appsystem.admin.model.dto.RubricDTO;
 import com.codecool.appsystem.admin.service.RubricService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -18,19 +18,19 @@ public class RubricController {
     private RubricService rubricService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<Rubric> getRubrics(@RequestParam String criteriaId){
-        return rubricService.getRubricsByCriteria(criteriaId);
+    public List<RubricDTO> getRubrics(@RequestParam String criteriaId){
+        return rubricService.getRubricsDTOByCriteria(criteriaId);
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public boolean saveRubricList(@RequestBody List<Rubric> rubrics){
+    public boolean saveRubricList(@RequestBody List<RubricDTO> rubrics){
         log.debug(String.valueOf(rubrics.size()));
         rubricService.saveRubrics(rubrics);
         return true;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public boolean deleteRubric(@RequestParam Integer id){
+    public boolean deleteRubric(@PathVariable Integer id){
         rubricService.deleteRubric(id);
         return true;
     }
