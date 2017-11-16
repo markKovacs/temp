551c82190da748f86cdb32bfacf93fdbf0accc0a
castarox <mika.kamil12@gmail.com>
2017-11-16 09:04:01 +0100

    Create relation between Rubric and ScreeningStepCriteria

--- backend/src/main/java/com/codecool/appsystem/admin/model/Rubric.java
+++ backend/src/main/java/com/codecool/appsystem/admin/model/Rubric.java
@@ -1,19 +1,27 @@
 package com.codecool.appsystem.admin.model;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Data
 @Entity
+@EqualsAndHashCode(of = "id")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Rubric {
 
     @Id
     @SequenceGenerator(sequenceName = "rubric_id_seq", name = "entity_id_sequence")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "entity_id_sequence")
     private Integer id;
-
-    private String criteriaId;
+    @ManyToOne
+    @JoinColumn(name = "criteria_id", referencedColumnName = "id")
+    @JsonIgnore
+    private ScreeningStepCriteria criteria;
     private String text;
 
     @Column(name = "rubric_order")
